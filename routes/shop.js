28052815f24553c706ee588f2033e4e8bf457b51
Abubakar Dubagari <superango93@gmail.com>
2024-01-03 15:22:11 +0100

    add to cart with user modules

---
+++ routes/shop.js
@@ -0,0 +1,21 @@
+/** @format */
+
+const express = require("express");
+
+const shopController = require("../controller/shop");
+
+const routes = express.Router();
+
+routes.get("/", shopController.getIndex);
+
+routes.get("/product-list", shopController.getproductlis);
+
+routes.get("/product-details/:productId", shopController.getProductDtails);
+
+// routes.get("/cart", shopController.getCart);
+
+routes.post("/cart", shopController.postCart);
+
+// routes.post("/cart-delete-items", shopController.postCartDelete);
+
+module.exports = routes;
